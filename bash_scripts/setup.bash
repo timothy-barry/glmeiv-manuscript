b6151e36e610a4bd7d5d29303173780fb7cb44c3
Tim <tbarry2@andrew.cmu.edu>
2021-08-10 21:34:57 -0400

    setup.sh

---
+++ bash_scripts/setup.bash
@@ -0,0 +1,22 @@
+# sets up the offsite directory structure
+source ~/.research_config
+
+# 1. Create a nextflow config file, which puts the "work"
+# directory in the top-level glmeiv data directory.
+if [ -f nextflow.config ]; then
+  rm nextflow.config
+fi
+touch nextflow.config
+echo workDir = \"$LOCAL_GLMEIV_DATA_DIR\work\" >> nextflow.config
+
+# 2. Initialize several directories offsite:
+# - simulations/{spec_objects, results}
+# - gasperini/{data, results}
+# - xie/{data, results}
+public_dir=$LOCAL_GLMEIV_DATA_DIR"public/"
+mkdir -p $public_dir"simulations/spec_objects" \
+$public_dir"simulations/results" \
+$public_dir"gasperini/data" \
+$public_dir"gasperini/results" \
+$public_dir"xie/data" \
+$public_dir"xie/results"
