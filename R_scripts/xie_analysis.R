4ab752a49132e7fd08ee2ef197ac65fcfdb24e88
Tim <tbarry2@andrew.cmu.edu>
2021-08-27 16:52:14 -0400

    add plotting code for xie analysis

--- R_scripts/xie_analysis.R
+++ R_scripts/xie_analysis.R
@@ -8,11 +8,7 @@ res_sum <- res %>% filter(type == "neg_control", parameter == "m_perturbation") 
   select(target, value, pair_id) %>% pivot_wider(id_cols = "pair_id", names_from = "target", values_from = "value") %>%
   mutate(cover = (confint_lower <= 1 & confint_upper >= 1))
 
-res_sum_cis <- res %>% filter(type == "cis", parameter == "m_perturbation") %>% 
-  select(target, value, pair_id) %>% pivot_wider(id_cols = "pair_id", names_from = "target", values_from = "value") %>%
-  mutate(cover = (confint_lower <= 1 & confint_upper >= 1)) %>% filter(estimate < 1.5, !is.na(confint_lower))
-
-# res sum
+# res sum (glmeiv)
 res_sum <- filter(res_sum, estimate < 1.5, !is.na(confint_lower))
 hist(res_sum$estimate, breaks = 30, freq = FALSE, xlim = c(0.75, 1.25))
 abline(v = 1, col = "red")
@@ -21,9 +17,18 @@ mu <- mean(ests); sigma <- sd(ests)
 xs <- seq(0.6, 1.4, length.out = 1000)
 ys <- dnorm(x = xs, mean = mu, sd = sigma)
 lines(x = xs, y = ys, col = "blue")
-res_sum %>% summarize(coverage = mean(cover, na.rm = TRUE),
-                      p_below_0.8 = 100 * mean(estimate < 0.8))
+stat_eiv <- res_sum %>% summarize(coverage = mean(cover, na.rm = TRUE),
+                                  ci_width = mean(confint_upper - confint_lower, na.rm = TRUE),
+                                  ci_width_log = mean(log(confint_upper) - log(confint_lower), na.rm = TRUE))
 
-# res sum cis
-res_sum_cis %>% summarize(coverage = mean(cover, na.rm = TRUE),
-                          p_below_0.8 = 100 * mean(estimate < 0.8))
+
+res_thresh <- readRDS(paste0(glmeiv_offsite_dir, "xie/results/result_thresholding.rds"))
+res_sum_thresh <- res_thresh %>% filter(type == "neg_control", parameter == "m_perturbation") %>%
+  select(target, value, pair_id) %>% pivot_wider(id_cols = "pair_id", names_from = "target", values_from = "value") %>%
+  mutate(cover = (confint_lower <= 1 & confint_upper >= 1))
+stat_thresh <- res_sum_thresh %>% summarize(coverage = mean(cover, na.rm = TRUE),
+                                            ci_width = mean(confint_upper - confint_lower, na.rm = TRUE),
+                                            ci_width_log = mean(log(confint_upper) - log(confint_lower), na.rm = TRUE))
+
+(stat_thresh$ci_width_log - stat_eiv$ci_width_log)/stat_thresh$ci_width_log * 100
+# 4% decrease in CI width (on untransformed scale), with greater CI coverage.
