9c8dc01922fea4b926934d98cdbc2991d629fc43
Tim <tbarry2@andrew.cmu.edu>
2021-10-13 20:49:10 -0400

    update background section

--- R_scripts/plotting/thresholding_theoretical.R
+++ R_scripts/plotting/thresholding_theoretical.R
@@ -36,10 +36,28 @@ pi <- seq(0.0, 0.5, length.out = 11)
 Estimate <- 1 - pi
 to_plot_b <- data.frame(pi = pi, Estimate = Estimate)
 
-# subplot c) variance as c -> infinity
-ggplot(data = to_plot_b, mapping = aes(x = pi, y = Estimate)) + geom_line() +
+p_b <- ggplot(data = to_plot_b, mapping = aes(x = pi, y = Estimate)) + geom_line() +
   scale_x_continuous(trans = "reverse", expand = c(NA, 0), limits = c(0.5, -0.02)) + theme_cowplot(font_size = 11) +
   geom_segment(aes(x=0.0, xend=0.5, y=1, yend=1), lwd = 0.3) + scale_y_continuous(expand = c(0, NA), limits = c(0.5, 1.02)) +
   xlab(expression(pi))
 
-# 
+# subplot c) bias-variance tradeoff
+xgrid <- seq(1, 3.9, length.out = 1001)
+g_beta <- 1
+pi <- 0.25
+m_beta <- 1
+n <- 10000
+
+bias_sq <- (1 - get_thresholding_estimator_est_no_int(c = xgrid, g_beta = g_beta, pi = pi, m_beta = m_beta))^2
+var <- get_thresholding_estimator_var_no_int(c = xgrid, g_beta = g_beta, pi = pi, m_beta = m_beta, n = n)
+to_plot_c <- data.frame(Metric = rep(c("Bias^2", "Variance", "MSE"), each = length(xgrid)),
+                        value = c(bias_sq, var, bias_sq + var),
+                        threshold = rep(xgrid, times = 3))
+p_c <- ggplot(data = to_plot_c, mapping = aes(x = threshold, y = value, col = Metric)) + geom_line() + theme_cowplot(font_size = 11) + xlab("Threshold") + ylab("") +
+  scale_color_manual(values = c("black", my_cols[1], my_cols[2]), labels = c("MSE", expression(Bias^2), "Variance"), breaks = c("MSE", expression(Bias^2), "Variance"))
+
+
+# combine
+p_bottom <- plot_grid(p_b, p_c, align = "h", ncol = 2, rel_widths = c(1,2), labels = c("b", "c"))
+p <- plot_grid(p_a, p_bottom, ncol = 1, labels = c("a", ""))
+
