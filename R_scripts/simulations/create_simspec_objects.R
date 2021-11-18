246a60ebec45c938e5ca93f43aec3dd79aa47439
Tim <tbarry2@andrew.cmu.edu>
2021-11-17 22:05:01 -0500

    create sim spec 0

--- R_scripts/simulations/create_simspec_objects.R
+++ R_scripts/simulations/create_simspec_objects.R
@@ -1,5 +1,5 @@
 library(magrittr)
-library(glmeiv)
+load_all("~/research_code/glmeiv/")
 library(simulatr)
 
 args <- commandArgs(trailingOnly = TRUE)
@@ -15,7 +15,64 @@ save_obj <- function(obj, file_path, overwrite) {
 }
 sim_dir <- paste0(.get_config_path("LOCAL_GLMEIV_DATA_DIR"), "public/simulations/spec_objects")
 
+one_rep_times <- list(generate_data_function = NA_integer_,
+                      thresholding = NA_integer_,
+                      glmeiv_fast = NA_integer_,
+                      glmeiv_slow = NA_integer_)
 
+##############################
+# Experiment 0
+# Vary g_pert
+# Fix distribution to Poisson
+# All three methods
+# One covariate (batch)
+##############################
+set.seed(4)
+theta <- 20
+n <- 150000
+to_save_sim_1 <- paste0(sim_dir, "/sim_spec_0.rds")
+g_perturbation_grid <- log(seq(1, 3.5, 0.5))
+param_grid <- expand.grid(g_perturbation = g_perturbation_grid)
+param_grid$grid_id <- seq(1, nrow(param_grid))
+
+fixed_params <- list(
+  m_fam = poisson() %>% augment_family_object(),
+  g_fam = poisson() %>% augment_family_object(),
+  seed = 4,
+  n = n,
+  B = 500,
+  m_intercept = log(0.01),
+  m_perturbation = log(0.25),
+  g_intercept = log(0.005),
+  covariate_matrix = data.frame(batch = rbinom(n = n, size = 1, prob = 0.5)),
+  m_covariate_coefs = log(0.9),
+  g_covariate_coefs = log(1.1),
+  n_processors = 20,
+  alpha = 0.95,
+  n_em_rep = 15,
+  save_membership_probs_mult = 500,
+  pi = 0.02,
+  m_offset = log(rpois(n = n, lambda = 10000)),
+  g_offset = log(rpois(n = n, lambda = 5000)),
+  pi_guess_range = c(1e-5, 0.03),
+  m_perturbation_guess_range = log(c(0.1, 1.5)),
+  g_perturbation_guess_range = log(c(0.5, 10)),
+  m_intercept_guess_range = log(c(1e-4, 1e-1)),
+  g_intercept_guess_range = log(c(1e-4, 1e-1)),
+  m_covariate_coefs_guess_range = log(c(0.25, 2)),
+  g_covariate_coefs_guess_range = log(c(0.25, 2)),
+  run_unknown_theta_precomputation = FALSE)
+
+sim_spec_0 <- create_simulatr_specifier_object(param_grid = param_grid,
+                                               fixed_params = fixed_params,
+                                               one_rep_times = one_rep_times,
+                                               methods = c("glmeiv_fast", "glmeiv_slow", "thresholding"))
+# check <- simulatr::check_simulatr_specifier_object(simulatr_spec = sim_spec_0, B_in = 2, parallel = TRUE)
+save_obj(obj = sim_spec_0, file_path = paste0(sim_dir, "/sim_spec_0.rds"), overwrite = overwrite)
+
+
+# TURN OFF EVERYTHING BELOW:
+if (FALSE) {
 ####################################################################
 # Experiment 1
 # Vary g_pert, fix all other parameters
@@ -64,11 +121,6 @@ fixed_params <- list(
   m_covariate_coefs_guess_range = log(c(0.25, 2)),
   g_covariate_coefs_guess_range = log(c(0.25, 2)))
 
-one_rep_times <- list(generate_data_function = NA_integer_,
-                      thresholding = NA_integer_,
-                      glmeiv_fast = NA_integer_,
-                      glmeiv_slow = NA_integer_)
-
 sim_spec_1 <- create_simulatr_specifier_object(param_grid = param_grid,
                                                fixed_params = fixed_params,
                                                one_rep_times = one_rep_times,
@@ -83,5 +135,50 @@ save_obj(obj = sim_spec_1, file_path = paste0(sim_dir, "/sim_spec_1.rds"), overw
 # Two covariates: batch and library size
 ########################################
 set.seed(4)
-n <- 150000
-to_save_sim_2 <- paste0(sim_dir, "/sim_spec_1.rds")
+n <- 100000
+to_save_sim_2 <- paste0(sim_dir, "/sim_spec_2.rds")
+# g_perturbation_grid <- seq(0, 4, 0.5)
+g_perturbation_grid <- 2
+param_grid <- data.frame(g_perturbation = g_perturbation_grid,
+                         grid_id = seq(1, length(g_perturbation_grid)))
+fixed_params <- list(
+  m_fam = gaussian() %>% augment_family_object(),
+  g_fam = gaussian() %>% augment_family_object(),
+  seed = 4,
+  n = n,
+  B = 500,
+  m_intercept = 3,
+  m_perturbation = -5,
+  g_intercept = 1,
+  covariate_matrix = data.frame(lib_size = rpois(n = n, lambda = 5000)),
+  m_covariate_coefs = 5/5000,
+  g_covariate_coefs = -5/5000,
+  n_processors = 20,
+  alpha = 0.95,
+  n_em_rep = 25,
+  save_membership_probs_mult = 500,
+  pi = 0.1,
+  m_offset = NULL,
+  g_offset = NULL,
+  pi_guess_range = c(1e-5, 0.03),
+  m_perturbation_guess_range = c(-5, 1),
+  g_perturbation_guess_range = c(-1, 5),
+  m_intercept_guess_range = c(0, 6),
+  g_intercept_guess_range = c(-2, 4),
+  m_covariate_coefs_guess_range = c(-0.5, 0.5),
+  g_covariate_coefs_guess_range = c(-0.5, 0.5),
+  run_unknown_theta_precomputation = FALSE)
+
+sim_spec_2 <- create_simulatr_specifier_object(param_grid = param_grid,
+                                               fixed_params = fixed_params,
+                                               one_rep_times = one_rep_times,
+                                               methods = c("glmeiv_fast", "thresholding"))
+
+check <- simulatr::check_simulatr_specifier_object(simulatr_spec = sim_spec_2, B_in = 2, parallel = TRUE)
+fit <- lm(formula = m ~ p + lib_size, data = mutate(check$data[[1]][[1]], fixed_params$covariate_matrix))
+# p <- check$data[[1]][[1]]$p
+# lib_size <- rpois(n = n, lambda = 10000)
+# ep <- rnorm(length(p))
+# y <- 3 - 5 * p + 0.2 * lib_size + ep
+# fit <- lm(y ~ p + lib_size)
+}
