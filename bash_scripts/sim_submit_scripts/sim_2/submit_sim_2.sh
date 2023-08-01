f9088e8d7fdd4e7c60ecf55753a8cad731ec8c13
Tim <tbarry2@andrew.cmu.edu>
2023-08-01 18:00:19 -0400

    fix typo in submit sim 2

--- bash_scripts/sim_submit_scripts/sim_2/submit_sim_2.sh
+++ bash_scripts/sim_submit_scripts/sim_2/submit_sim_2.sh
@@ -2,7 +2,7 @@
 source ~/.research_config
 sim_dir=$LOCAL_GLMEIV_DATA_DIR"public/simulations/"
 result_dir=$sim_dir"results/"
-spec_obj_1=$sim_dir"spec_objects/sim_spec_2.rds"
+spec_obj_2=$sim_dir"spec_objects/sim_spec_2.rds"
 
 nextflow pull timothy-barry/simulatr-pipeline
 nextflow run timothy-barry/simulatr-pipeline \
