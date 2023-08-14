8b4802a119ca6ca8ee38068b3b54bff3e37c1def
Tim <tbarry2@andrew.cmu.edu>
2023-08-14 01:20:59 -0400

    prep sim 6

--- R_scripts/simulations/create_simspec_objects.R
+++ R_scripts/simulations/create_simspec_objects.R
@@ -285,7 +285,7 @@ save_obj(obj = sim_spec_5, file_path = paste0(sim_dir, "/sim_spec_5.rds"), overw
 set.seed(4)
 theta <- 20
 n <- 50000
-g_perturbation_grid <- log(seq(1, 4, 0.5))
+g_perturbation_grid <- log(seq(1.5, 4, 0.5))
 m_perturbation_grid <- log(seq(1, 0.25, by = -0.25))
 
 param_grid <- expand.grid(g_perturbation = g_perturbation_grid,
@@ -305,8 +305,8 @@ fixed_params <- list(
   g_intercept = log(0.005),
   covariate_matrix = data.frame(batch = rbinom(n = n, size = 1, prob = 0.5),
                                 cell_cycle = runif(n = n, min = 0, max = 1)),
-  grna_duplet_rate = 0.0,
-  mrna_duplet_rate = 0.02,
+  grna_duplet_rate = 0.00,
+  mrna_duplet_rate = 0.01,
   rm_covariate = "cell_cycle",
   m_covariate_coefs = log(c(0.9, 1.25)),
   g_covariate_coefs = log(c(0.8, 1.0)),
